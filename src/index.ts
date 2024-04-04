5e16b47bd5fdcaaec28b2b52d48447e6036eb607
Fabio Spampinato <spampinabio@gmail.com>
2024-04-04 23:40:35 +0100

    Ensuring the Yielder type is exported

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@
 /* IMPORT */
 
 import {makeImmediateYielder, makeIntervalYielder, makeTimeoutYielder} from '~/yielders';
+import type {Yielder} from '~/types';
 
 /* EXPORT */
 
 export {makeImmediateYielder, makeIntervalYielder, makeTimeoutYielder};
+export type {Yielder};
